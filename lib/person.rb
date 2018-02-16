34bbd0aead226ebebbcd15b7fc95a47b14760e2c
Eden Mesfun <emesfun@gmail.com>
2018-02-16 14:19:22 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -7,18 +7,18 @@ def initialize(name:, birthday:, hair_color:, eye_color:,
               height:, weight:, handed:, complexion:,
               t_shirt_size:, wrist_size:, glove_size:, pant_length:, pant_width:)
 
-    @name = name 
+    @name = name
     @birthday = birthday
-    @hair_color = hair_color 
-    @eye_color = eye_color 
+    @hair_color = hair_color
+    @eye_color = eye_color
     @height = height
-    @weight = weight 
-    @handed = handed 
-    @complexion = complexion 
+    @weight = weight
+    @handed = handed
+    @complexion = complexion
     @t_shirt_size = t_shirt_size
-    @wrist_size = wrist_size 
-    @glove_size = glove_size 
-    @pant_length = pant_length 
+    @wrist_size = wrist_size
+    @glove_size = glove_size
+    @pant_length = pant_length
     @pant_width = pant_width
 
 end
