6fa64fae9546810dc6e21a4b2c75a32eb17f2d98
Eden Mesfun <emesfun@gmail.com>
2018-02-16 14:12:26 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -4,9 +4,9 @@ class Person
                 :t_shirt_size,:wrist_size, :glove_size, :pant_length, :pant_width
 
 def initialize
-  attributes = {:name, :birthday, :hair_color, :eye_color,
-                :height, :weight, :handed, :complexion,
-                :t_shirt_size,:wrist_size, :glove_size, :pant_length, :pant_width}
+  attributes = { name:, birthday:, hair_color:, eye_color:,
+                height:, weight:, handed:, complexion:,
+                t_shirt_size:, wrist_size:, glove_size:, pant_length:, pant_width:}
 end
 
 end
