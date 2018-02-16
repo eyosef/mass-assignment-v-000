f669e035eeffa1d6228cb2c0c79c1209bd1a7761
Eden Mesfun <emesfun@gmail.com>
2018-02-16 14:40:34 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -4,10 +4,9 @@ class Person
                 :t_shirt_size,:wrist_size, :glove_size, :pant_length, :pant_width
 
       def initialize(params)
-        params.each do |key, value|
-          instance_variable_set("@#{key}", value)
-        end
-
+          params.each do |key, value|
+            instance_variable_set("@#{key}", value)
+          end
       end
 
 end
