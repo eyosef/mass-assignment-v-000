ffade1260e90abc52f21ee2583c46ec1bb9bac77
Eden Mesfun <emesfun@gmail.com>
2018-02-16 14:40:15 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -8,20 +8,6 @@ class Person
           instance_variable_set("@#{key}", value)
         end
 
-              @name = name
-              @birthday = birthday
-              @hair_color = hair_color
-              @eye_color = eye_color
-              @height = height
-              @weight = weight
-              @handed = handed
-              @complexion = complexion
-              @t_shirt_size = t_shirt_size
-              @wrist_size = wrist_size
-              @glove_size = glove_size
-              @pant_length = pant_length
-              @pant_width = pant_width
-
       end
 
 end
