4a83dfb98f22be5822c465af721365f1983ba9d0
Eden Mesfun <emesfun@gmail.com>
2018-02-16 14:10:53 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -3,8 +3,10 @@ class Person
                 :height, :weight, :handed, :complexion,
                 :t_shirt_size,:wrist_size, :glove_size, :pant_length, :pant_width
 
-def initialize 
-  attributes = Hash.new(0)
-end 
+def initialize
+  attributes = {:name, :birthday, :hair_color, :eye_color,
+                :height, :weight, :handed, :complexion,
+                :t_shirt_size,:wrist_size, :glove_size, :pant_length, :pant_width}
+end
 
 end
